5d6674fcd546b0ba46b651a8bf0787b180fcbaea
Kai Herzig <kihe@mms-t-systems.de>
2019-07-09 09:30:30 +0200

    added provider

---
+++ LabEnvironment/variables.tf
@@ -0,0 +1,12 @@
+variable "provider_subscription_id" {
+  description = "Subscription ID"
+}
+variable "provider_client_id" {
+  description = "Application ID"
+}
+variable "provider_client_secret" {
+  description = "Application Token"
+}
+variable "provider_tenant_id" {
+  description = "Azure Active Directory ID"
+}
