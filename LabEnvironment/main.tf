3865890873fc9f41cbb741cc5413bbc771f058a7
Kai Herzig <kihe@mms-t-systems.de>
2019-07-09 15:26:53 +0200

    fixed variable naming

--- LabEnvironment/main.tf
+++ LabEnvironment/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "TFWS_Labstations" {
 
 resource "azurerm_virtual_network" "TFWS_Labstations-vn" {
   name                = "${var.rg_labstation}-vn"
-  address_space       = ["${var.vn_cidr}"]
+  address_space       = ["${var.vn_labstations_cidr}"]
   location            = "${azurerm_resource_group.TFWS_Labstations.location}"
   resource_group_name = "${azurerm_resource_group.TFWS_Labstations.name}"
  }
